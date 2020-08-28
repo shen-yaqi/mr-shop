950adb11f7b0fc9721ff67baada489e2ffaffca8
shenyaqi <shenyaqiii@163.com>
2020-08-28 17:49:09 +0800

    分类新增-删除-修改

--- mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -1,11 +1,14 @@
 package com.baidu.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.CategoryService;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -34,5 +37,64 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
 
         //return new Result<List<CategoryEntity>>(HTTPStatus.OK,"",list);
         return this.setResultSuccess(list);
+    }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> saveCategory(CategoryEntity categoryEntity) {
+
+        //通过页面传递过来的parentid查询parentid对应的数据是否为父节点isParent==1
+        //如果parentid对应的isParent != 1
+        //需要修改为1
+
+        //通过新增节点的父id将父节点的parent状态改为1
+        CategoryEntity parentCateEntity = new CategoryEntity();
+        parentCateEntity.setId(categoryEntity.getParentId());
+        parentCateEntity.setIsParent(1);
+        categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+
+        categoryMapper.insertSelective(categoryEntity);
+
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> editCategory(CategoryEntity categoryEntity) {
+
+        categoryMapper.updateByPrimaryKeySelective(categoryEntity);
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> deleteCategory(Integer id) {
+
+        //验证传入的id是否有效,并且查询出来的数据对接下来的程序有用
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+        if (categoryEntity == null) {
+            return this.setResultError("当前id不存在");
+        }
+        //判断当前节点是否为父节点
+        if(categoryEntity.getIsParent() == 1){
+            return this.setResultError("当前节点为父节点,不能删除");
+        }
+
+        //构建条件查询 通过当前被删除节点的parentid查询数据
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+        //如果查询出来的数据只有一条
+        if(list.size() == 1){//将父节点的isParent状态改为0
+
+            CategoryEntity parentCateEntity = new CategoryEntity();
+            parentCateEntity.setId(categoryEntity.getParentId());
+            parentCateEntity.setIsParent(0);
+            categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+        }
+
+        categoryMapper.deleteByPrimaryKey(id);//执行删除
+
+        return this.setResultSuccess();
     }
 }
