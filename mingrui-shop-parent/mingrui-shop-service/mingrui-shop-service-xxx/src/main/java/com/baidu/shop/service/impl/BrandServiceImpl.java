233adf03dd12a02f520728b04a3c32efb49079ee
shenyaqi <shenyaqiii@163.com>
2020-08-31 17:07:42 +0800

    条件查询

--- mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/BrandServiceImpl.java
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/BrandServiceImpl.java
@@ -6,11 +6,9 @@ import com.baidu.shop.dto.BrandDTO;
 import com.baidu.shop.entity.BrandEntity;
 import com.baidu.shop.mapper.BrandMapper;
 import com.baidu.shop.service.BrandService;
-import com.baidu.shop.utils.ObjectUtil;
 import com.baidu.shop.utils.StringUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
 import tk.mybatis.mapper.entity.Example;
 
@@ -33,16 +31,27 @@ public class BrandServiceImpl extends BaseApiService implements BrandService {
     @Override
     public Result<PageInfo<BrandEntity>> getBrandInfo(BrandDTO brandDTO) {
 
+        //分页
         PageHelper.startPage(brandDTO.getPage(),brandDTO.getRows());
 
+        //排序/条件查询
         Example example = new Example(BrandEntity.class);
-
         if(StringUtil.isNotEmpty(brandDTO.getSort())) example.setOrderByClause(brandDTO.getOrderByClause());
+//        Example.Criteria criteria = example.createCriteria();
+//        //条件查询
+//        if (StringUtil.isNotEmpty(brandDTO.getName())) criteria.andLike("name","%" + brandDTO.getName() + "%");
 
+        if (StringUtil.isNotEmpty(brandDTO.getName())) example.createCriteria()
+                .andLike("name","%" + brandDTO.getName() + "%");
+
+
+        //查询
         List<BrandEntity> list = brandMapper.selectByExample(example);
 
+        //数据封装
         PageInfo<BrandEntity> pageInfo = new PageInfo<>(list);
 
+        //返回
         return this.setResultSuccess(pageInfo);
     }
 }
