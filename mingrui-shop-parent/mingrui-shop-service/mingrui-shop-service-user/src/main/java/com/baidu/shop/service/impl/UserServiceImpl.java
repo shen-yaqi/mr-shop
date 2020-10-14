da2bc5acd85d45eaf8c9ef31884b651b87583bb8
shenyaqi <shenyaqiii@163.com>
2020-10-14 11:40:47 +0800

    项目集成redis

--- mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-user/src/main/java/com/baidu/shop/service/impl/UserServiceImpl.java
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-user/src/main/java/com/baidu/shop/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.baidu.shop.constant.UserConstant;
 import com.baidu.shop.dto.UserDTO;
 import com.baidu.shop.entity.UserEntity;
 import com.baidu.shop.mapper.UserMapper;
+import com.baidu.shop.redis.repository.RedisRepository;
 import com.baidu.shop.service.UserService;
 import com.baidu.shop.utils.BCryptUtil;
 import com.baidu.shop.utils.BaiduBeanUtil;
@@ -33,6 +34,9 @@ public class UserServiceImpl extends BaseApiService implements UserService {
     @Resource
     private UserMapper userMapper;
 
+    @Resource
+    private RedisRepository redisRepository;
+
     @Override
     public Result<JSONObject> register(UserDTO userDTO) {
         //注册
@@ -43,6 +47,7 @@ public class UserServiceImpl extends BaseApiService implements UserService {
         userEntity.setPassword(BCryptUtil.hashpw(userEntity.getPassword(),BCryptUtil.gensalt()));
 
         userMapper.insertSelective(userEntity);
+
         return this.setResultSuccess();
     }
 
@@ -50,7 +55,6 @@ public class UserServiceImpl extends BaseApiService implements UserService {
     public Result<List<UserEntity>> checkUsernameOrPhone(String value, Integer type) {
         //value: admin
         //value: 18888888888
-
         //type:1 select * from tb_user where username='admin'
         //type:2 select * from tb_user where phone='18888888888'
         Example example = new Example(UserEntity.class);
@@ -72,7 +76,7 @@ public class UserServiceImpl extends BaseApiService implements UserService {
     public Result<JSONObject> sendValidCode(UserDTO userDTO) {
         //验证码随机六位
         String code = (int)((Math.random() * 9 + 1) * 100000) + "";
-
+        //第三方SDK 支付宝 微信 短信
         log.debug("发送短信验证码-->手机号 : {} , 验证码 : {}",userDTO.getPhone(),code);
         //LuosimaoDuanxinUtil.sendSpeak(userDTO.getPhone(),code);
         return this.setResultSuccess();
